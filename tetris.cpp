92fbfc896b14c9178cb861dc079e0bf5d75ae9da
Francisco José Escabias Ortega <fjeo0002@red.ujaen.es>
2024-03-29 13:09:02 +0100

    pintarPieza() + ejemplos

--- tetris.cpp
+++ tetris.cpp
@@ -11,36 +11,49 @@ const int FILAS = 10;
  * @brief Dibuja un cuadrado relleno en la pantalla.
  *
  * @post Esta función dibuja un cuadrado relleno en la posición especificada
- * con el color especificado.
  *
  * @param x Coordenada x de la esquina superior izquierda del cuadrado.
  * @param y Coordenada y de la esquina superior izquierda del cuadrado.
- * @param rgb Color en para el relleno del cuadrado.
  *
  * @return void
  *
  * @see color, rectangulo_lleno
  */
-void cuadrado(int x, int y, int rgb) {
-    color(rgb);
+void cuadrado(int x, int y) {
     rectangulo_lleno(x * TAM,
                      y * TAM,
                      x * TAM + TAM,
                      y * TAM + TAM);
 }
 
+/**
+ * @brief Dibuja una pieza tipo S1 en la pantalla.
+ *
+ * Esta función dibuja una pieza tipo S1 compuesta por cuatro cuadrados
+ * de color rojo en la posición especificada.
+ *
+ * @param x Coordenada x del punto de inicio de la pieza.
+ * @param y Coordenada y del punto de inicio de la pieza.
+ *
+ * @return void
+ *
+ * @see color, cuadrado
+ */
+void pieza_s1(int x, int y) {
+    color(ROJO);
+    cuadrado(x, y);
+    cuadrado(x + 1, y);
+    cuadrado(x + 1, y + 1);
+    cuadrado(x + 2, y + 1);
+}
+
 int main() {
     //Dibujamos el tablero de 10*20;
     vredimensiona(TAM * FILAS, TAM * COLUMNAS);
-    //Ejemplos para probar la nueva función cuadrado
-    cuadrado(0, 0, ROJO);
-    cuadrado(1, 0, VERDE);
-    cuadrado(2, 0, AMARILLO);
-    cuadrado(0, 2, AZUL);
+    //Ejemplos para probar la nueva función pieza_s1
+    pieza_s1(5,5);
+    pieza_s1(3,10);
 
-    for (int i = 0; i < 10; ++i) {
-        cuadrado(9, i*2, BLANCO);
-    }
     //Al pintar siempre refresca la pantalla
     refresca();
     return 0;
