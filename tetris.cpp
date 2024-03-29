68e63441a34b2de5e82d8980d1e3afd6234938c9
Francisco José Escabias Ortega <fjeo0002@red.ujaen.es>
2024-03-29 13:20:52 +0100

    Interaccion de Mvto con Pieza

--- tetris.cpp
+++ tetris.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 using namespace miniwin;
 
+/// Constantes:
+// Tamaño Relativo de la ventana
 const int TAM = 25;
+// Numero de Columnas y Filas
 const int COLUMNAS = 20;
 const int FILAS = 10;
 
@@ -20,8 +23,8 @@ const int FILAS = 10;
  * @see color, rectangulo_lleno
  */
 void cuadrado(int x, int y) {
-    rectangulo_lleno(x * TAM,
-                     y * TAM,
+    rectangulo_lleno(1 + x * TAM, // 1 + --> damos borde
+                     1 + y * TAM,
                      x * TAM + TAM,
                      y * TAM + TAM);
 }
@@ -50,11 +53,38 @@ void pieza_s1(int x, int y) {
 int main() {
     //Dibujamos el tablero de 10*20;
     vredimensiona(TAM * FILAS, TAM * COLUMNAS);
-    //Ejemplos para probar la nueva función pieza_s1
-    pieza_s1(5,5);
-    pieza_s1(3,10);
 
-    //Al pintar siempre refresca la pantalla
+    //Pintamos la pieza en (0,0)
+    int x = 0, y = 0;
+    pieza_s1(x, y);
     refresca();
+    //Accionamos una tecla
+    int t = tecla();
+    //Pulsar ESCAPE para cerrar la ventana
+    while (t != ESCAPE) {
+        //Movemos la pieza
+        if (t == ARRIBA) {
+            y--;
+        } else if (t == ABAJO) {
+            y++;
+        } else if (t == DERECHA) {
+            x++;
+        } else if (t == IZQUIERDA) {
+            x--;
+        }
+        //Cuando aprietas una tecla, refrescas la pantalla
+        //1. Borramos la pieza anterior
+        //2. Dibujamos la nueva
+        //3. Refrescamos la pantalla
+        if(t != NINGUNA){
+            borra();
+            pieza_s1(x, y);
+            refresca();
+        }
+        //Pulsamos una nueva tecla
+        t = tecla();
+    }
+    vcierra();
+
     return 0;
 }
