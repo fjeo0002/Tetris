37653f5906b1e20181a79a00484708c9f313409c
Francisco José Escabias Ortega <fjeo0002@red.ujaen.es>
2024-04-04 13:40:24 +0200

    Deteccion de Colision

--- tetris.cpp
+++ tetris.cpp
@@ -17,6 +17,12 @@ struct Pieza {
     Coord abs;
     Coord relat[3];
     int color;
+
+    //n entre 0 y 3 (0 = absoluto, entre 1 y 3 = relativos)
+    Coord posicionBloque(int n) const {
+        if (n == 0) return abs;
+        return {abs.x + relat[n - 1].x, abs.y + relat[n - 1].y};
+    }
 };
 
 typedef int Tablero[COLUMNAS][FILAS];
@@ -28,38 +34,38 @@ void cuadrado(int x, int y) {
                      y * TAM + TAM);
 }
 
-void pinta_pieza(const Pieza& P) {
+void pinta_pieza(const Pieza &P) {
     color(P.color);
     cuadrado(P.abs.x, P.abs.y);
-    for (int i = 0; i < 3; ++i) {
-        cuadrado(P.abs.x + P.relat[i].x,
-                 P.abs.y + P.relat[i].y);
+    for (int i = 0; i < 4; ++i) {
+        Coord c = P.posicionBloque(i);
+        cuadrado(c.x, c.y);
     }
 }
 
-Coord rota_derecha(const Coord& c){
+Coord rota_derecha(const Coord &c) {
     Coord ret = {-c.y, c.x};
     return ret;
 }
 
-void rota_derecha(Pieza& P) {
+void rota_derecha(Pieza &P) {
     for (int i = 0; i < 3; ++i) {
         P.relat[i] = rota_derecha(P.relat[i]);
     }
 }
 
-Coord rota_izquierda(const Coord& c){
+Coord rota_izquierda(const Coord &c) {
     Coord ret = {c.y, -c.x};
     return ret;
 }
 
-void rota_izquierda(Pieza& P) {
+void rota_izquierda(Pieza &P) {
     for (int i = 0; i < 3; ++i) {
         P.relat[i] = rota_izquierda(P.relat[i]);
     }
 }
 
-void vaciarTablero(Tablero& T) {
+void vaciarTablero(Tablero &T) {
     for (int i = 0; i < COLUMNAS; ++i) {
         for (int j = 0; j < FILAS; ++j) {
             T[i][j] = NEGRO;
@@ -67,7 +73,7 @@ void vaciarTablero(Tablero& T) {
     }
 }
 
-void actualizaTablero(const Tablero& T) {
+void actualizaTablero(const Tablero &T) {
     for (int i = 0; i < COLUMNAS; ++i) {
         for (int j = 0; j < FILAS; ++j) {
             color(T[i][j]);
@@ -76,24 +82,36 @@ void actualizaTablero(const Tablero& T) {
     }
 }
 
-void insertaPieza(Tablero& T, const Pieza& P) {
-    T[P.abs.x][P.abs.y] = P.color;
-    for (int i = 0; i < 3; ++i) {
-        T[P.abs.x + P.relat[i].x][P.abs.y + P.relat[i].y] = P.color;
+void insertaPieza(Tablero &T, const Pieza &P) {
+    for (int i = 0; i < 4; ++i) {
+        Coord c = P.posicionBloque(i);
+        T[c.x][c.y] = P.color;
     }
+}
+
+bool colisionPieza(const Tablero &T, const Pieza &P) {
+    for (int i = 0; i < 4; ++i) {
+        Coord c = P.posicionBloque(i);
+        if (c.x < 0 || c.x >= COLUMNAS || c.y < 0 || c.y >= FILAS) {
+            return true;
+        } else if (T[c.x][c.y] != NEGRO) {
+            return true;
+        }
+    }
+    return false;
+
 }
 
 int main() {
     vredimensiona(TAM * COLUMNAS, TAM * FILAS);
 
-    Pieza P = {{4,8}, {{1,0},{1,1},{0,1}}, MAGENTA};
+    Pieza P = {{9, 10}, {{1, 0}, {1, 1}, {0, 1}}, MAGENTA};
 
     Tablero T;
     vaciarTablero(T);
-    insertaPieza(T,P);
-    borra();
-    actualizaTablero(T);
-    refresca();
+    if (!colisionPieza(T, P))
+        mensaje("No hay colision");
+    else mensaje("Hay colision");
 
     return 0;
 }
