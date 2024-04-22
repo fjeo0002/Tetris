13ff52dc93f7f5907cefbcc2abd8fbfb67d039d1
Francisco José Escabias Ortega <fjeo0002@red.ujaen.es>
2024-04-22 10:33:15 +0200

    Tetris_v3.0

--- tetris.cpp
+++ tetris.cpp
@@ -7,6 +7,9 @@ using namespace miniwin;
 const int TAM = 25;
 const int FILAS = 20;
 const int COLUMNAS = 10;
+const int MARGEN = 10;
+const int ANCHO = TAM * COLUMNAS;
+const int ALTO = TAM * FILAS;
 
 struct Coord {
     int x;
@@ -28,10 +31,10 @@ struct Pieza {
 typedef int Tablero[COLUMNAS][FILAS];
 
 void cuadrado(int x, int y) {
-    rectangulo_lleno(1 + x * TAM,
-                     1 + y * TAM,
-                     x * TAM + TAM,
-                     y * TAM + TAM);
+    rectangulo_lleno(MARGEN + 1 + x * TAM,
+                     MARGEN + 1 + y * TAM,
+                     MARGEN + x * TAM + TAM,
+                     MARGEN + y * TAM + TAM);
 }
 
 void pinta_pieza(const Pieza &P) {
@@ -112,12 +115,12 @@ bool filaLlena(const Tablero &T, int fila) {
 }
 
 void quitarFila(Tablero &T, int fila) {
-    for(int j = fila; j > 0; --j) {
+    for (int j = fila; j > 0; --j) {
         for (int i = 0; i < COLUMNAS; ++i) {
             T[i][j] = T[i][j - 1];
         }
     }
-    for(int i = 0; i < COLUMNAS; ++i) {
+    for (int i = 0; i < COLUMNAS; ++i) {
         T[i][0] = NEGRO;
     }
 }
@@ -126,11 +129,10 @@ int cuentaFila(Tablero &T) {
     int fila = FILAS - 1;
     int cont = 0;
     while (fila >= 0) {
-        if(filaLlena(T, fila)) {
-            quitarFila(T,fila);
+        if (filaLlena(T, fila)) {
+            quitarFila(T, fila);
             ++cont;
-        }
-        else{
+        } else {
             --fila;
         }
     }
@@ -138,18 +140,18 @@ int cuentaFila(Tablero &T) {
 }
 
 const Coord relats[7][3] = {
-        {{1,0},{1,1},{0,1}}, // Cuadrado            ROJO
-        {{1,0},{0,1},{-1,1}}, // S                  VERDE
-        {{-1,0},{0,1},{1,1}}, // 2                  AZUL
-        {{0,-1},{0,1},{1,1}}, // L                  AMARILLO
-        {{0,-1},{0,1},{-1,1}}, // L invertida       MAGENTA
-        {{0,-1},{0,1},{0,2}}, // Palo               CYAN
-        {{-1,0},{0,1},{1,0}} // T                   BLANCO
+        {{1,  0},  {1, 1}, {0,  1}}, // Cuadrado            ROJO
+        {{1,  0},  {0, 1}, {-1, 1}}, // S                  VERDE
+        {{-1, 0},  {0, 1}, {1,  1}}, // 2                  AZUL
+        {{0,  -1}, {0, 1}, {1,  1}}, // L                  AMARILLO
+        {{0,  -1}, {0, 1}, {-1, 1}}, // L invertida       MAGENTA
+        {{0,  -1}, {0, 1}, {0,  2}}, // Palo               CYAN
+        {{-1, 0},  {0, 1}, {1,  0}} // T                   BLANCO
 };
 
-void piezaNueva(Pieza& P){
-    P.abs.x = 4;
-    P.abs.y = 1;
+void piezaNueva(Pieza &P) {
+    P.abs.x = 13;
+    P.abs.y = 3;
 
     //Pieza al azar
     int r = rand() % 7;
@@ -160,19 +162,51 @@ void piezaNueva(Pieza& P){
     P.color = r;
 }
 
+void repinta(const Tablero &T, const Pieza &P, const Pieza &N, int ptos, int level) {
+    borra();
+    actualizaTablero(T);
+
+    color(BLANCO);
+    linea(MARGEN + 0, MARGEN + 0, MARGEN + 0, MARGEN + ALTO);
+    linea(MARGEN + 0, MARGEN + ALTO, MARGEN + ANCHO, MARGEN + ALTO);
+    linea(MARGEN + ANCHO, MARGEN + 0, MARGEN + ANCHO, MARGEN + ALTO);
+    linea(MARGEN + 0, MARGEN + 0, MARGEN + ANCHO, MARGEN + 0);
+
+    texto(MARGEN * 2 + TAM * COLUMNAS, MARGEN * 3, "Pieza Siguiente:");
+
+    texto(MARGEN * 2 + TAM * COLUMNAS, MARGEN * 20, "Puntos: " + to_string(ptos));
+
+    texto(MARGEN * 2 + TAM * COLUMNAS, MARGEN * 30, "Nivel: " + to_string(level));
+
+    pinta_pieza(P);
+    pinta_pieza(N);
+    refresca();
+}
+
+const int NIVELES[7] = {0, 100, 300, 600, 1000, 1500, 2000};
+
+const int VELOCIDAD[7] = {30,25,20,15,10,5,1};
+
 int main() {
     srand(time(nullptr));
 
-    vredimensiona(TAM * COLUMNAS, TAM * FILAS);
+    vredimensiona(MARGEN * 20 + ANCHO, MARGEN * 2 + ALTO);
 
     Tablero T;
     vaciarTablero(T);
-    actualizaTablero(T);
 
     Pieza P;
     piezaNueva(P);
-    pinta_pieza(P);
-    refresca();
+    P.abs.x = 4;
+    P.abs.y = 1;
+
+    Pieza N;
+    piezaNueva(N);
+
+    int ptos = 0;
+    int level = 1;
+
+    repinta(T, P, N, ptos, level);
 
     int t = tecla();
     int frame = 0;
@@ -180,35 +214,49 @@ int main() {
     while (t != ESCAPE) {
         Pieza copia = P;
 
-        if(t == NINGUNA && frame > 30){
+        if (t == NINGUNA && frame > VELOCIDAD[level-1]) {
             frame = 0;
             t = ABAJO;
         }
 
-        if(t == ARRIBA) {
+        if (t == ARRIBA) {
             rota_derecha(P);
-        } else if(t == ABAJO) {
+        } else if (t == ABAJO) {
             P.abs.y++;
-        } else if(t == IZQUIERDA) {
+        } else if (t == IZQUIERDA) {
             P.abs.x--;
-        } else if(t == DERECHA) {
+        } else if (t == DERECHA) {
             P.abs.x++;
         }
 
         if (colisionPieza(T, P)) {
             P = copia;
-            if(t == ABAJO) {
-                insertaPieza(T,P);
+            if (t == ABAJO) {
+                insertaPieza(T, P);
                 int cont = cuentaFila(T);
-                piezaNueva(P);
+                switch (cont) {
+                    case 1: ptos += 100; break;
+                    case 2: ptos += 300; break;
+                    case 3: ptos += 500; break;
+                    case 4: ptos += 800; break;
+                }
+                if(NIVELES[level] <= ptos){
+                    level++;
+                }
+                P = N;
+                piezaNueva(N);
+                P.abs.x = 4;
+                P.abs.y = 1;
+
+                if(colisionPieza(T, P)){
+                    mensaje("GAME OVER");
+                    espera(UINT_MAX);
+                }
             }
         }
 
-        if(t != NINGUNA){
-            borra();
-            actualizaTablero(T);
-            pinta_pieza(P);
-            refresca();
+        if (t != NINGUNA) {
+            repinta(T, P, N, ptos, level);
         }
 
         espera(30);
